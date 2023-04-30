e4a39ea183b8ed52b60df7770aa316b241aae52a
Nandita Bhaskhar <nanbhas@stanford.edu>
2023-04-29 21:28:21 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="siaug",
+    version="0.0.1",
+    description="Exploring Image Augmentations for Siamese Representation Learning with Chest X-Rays",
+    author="Rogier van der Sluijs, Nandita Bhaskhar",
+    author_email="sluijs@stanford.edu, nanbhas@stanford.edu",
+    url="https://github.com/StanfordMIMI/siaug",
+    install_requires=[""],
+    packages=find_packages(),
+)
